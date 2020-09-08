89f3bad26b01d43ac71cf4d4feb7e5f4e21c37c9
nishanth pathi <pathinishanth24@gmail.com>
2020-09-08 13:30:03 +0530

    second commit

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    
     return model
 
 
@@ -28,7 +29,7 @@ def answer_question(question, answer_text):
     '''
     # ======== Tokenize ========
     # Apply the tokenizer to the input text, treating them as a text-pair.
-    input_ids = tokenizer.encode(question, answer_text)
+    input_ids = tokenizer.encode(question, answer_text, max_length=512)
 
     # Report how long the input sequence is.
     print('Query has {:,} tokens.\n'.format(len(input_ids)))
@@ -87,7 +88,7 @@ wrapper = textwrap.TextWrapper(width=80)
     
 #sprint(wrapper.fill(bert_abstract))
 
-bert_abstract = " Applied AI is a online course to learn practical machine learning concepts with job guarantee program,if you complete all the assignments we assist you for job gaurantee program,we conduct mock interviews aswell for job gaurantee program, Validity of the Course is one year, Fee for the Course is twenty five thousand rupees only, You will get a Certificate once you complete the Course, i3 Processor with 8Gb RAM atleast is sufficient to complete the Course, We respond to your queries regarding any topic with in twenty four hours, feel free to drop us a mail at team@appliedaicourse.com, We conduct Live-Session based on student's demand every weekend, We do have EMI facility for Fee"
+bert_abstract = " Applied AI Course is a complete online course, validity of the Applied AI Course is 365 days. A mentor is allocated to you once you complete 15 mandatory assignments. You will get a certificate once you complete all course assignments please check https://www.appliedaicourse.com/certificate/8fa2c6377d to find the sample certificate. The Applied AI course provides a good overview of most of the popular ML and Deep Learning algorithms. Applied AI course is also constantly evolving as machine learning that is Syllabus  is not fixed. The Applied AI Course keeps adding the new contents with time. The Applied AI Course conducts live sessions every week. laptop/system requirements with around 8Gb of RAM minimum and i7 processor with 4G graphics card from NVIDIA. laptop/system requirements to run deep learning programs are 1050Ti and above is preferable. You can also look into Google Colab for running the code. Average package depends on different factors like the candidate's educational background, skillset the candidate possesses, type of the company the candidate is trying for, type of projects/portfolio the candidate has worked on call us on 91-8106-920-029. Mail us to team@appliedaicourse.com . No prerequisites are required, all the concepts discussed have been intuited from a fundamental level to an advanced level. text mining/NLP(Natural Language Processing) covers simple Bag-Of-Words, TF-IDF to more complex Word2Vec and LSTM networks which are state-of-the-art in text mining. We are providing a job guarantee program in India and the USA. The only reason behind not providing this job guarantee program in other countries is that we don't have relationships with the companies in those countries. Notes are not provided to students. Students are expected to prepare their own notes so that they can retain more and prepare notes as per their way of learning. For implementing the projects we use sklearn for ML models and tf.Keras for DL models. For understanding tf-IDF vectorizer, BOW vectorizer, Bootstrapping, Gradient Descent algorithm we need to write code from scratch. Difficulty level for assignment are if you understand the theory well it is very easy to follow the step by step instructions given under each assignment. Course content of Applied AI course has 150+hours of industry focused and extremely simplified content it also includes applied aspects of artificial intelligence. we respond to your queries regarding any topic with in twenty four hours."
 
 question = st.text_input('Ask your question here:')
 
